f01d95de948451db1427f859f6a3bfaae85f5b5d
SarahZimmermann <sarah.zimmermann@neuronity.de>
2025-11-24 13:39:19 +0100

    fix: run isort and black

--- src/tsa_products/tests.py
+++ src/tsa_products/tests.py
@@ -1,34 +1,27 @@
-from django.test import TestCase  # noqa F401
-from tsa_products.models import Category
 from django.core.exceptions import ValidationError
+from django.test import TestCase  # noqa F401
 
+from tsa_products.models import Category
 
 # Create your tests here.
 
+
 class CategoryTestCase(TestCase):
-    
+
     def test_success_create_category(self):
         initial_number_of_categories = Category.objects.count()
-        
+
         test_category = Category.objects.create(
-            title="test",
-            image= "test",
-            base_price=0.0,
-            max_amount_of_lettering_items= -1,
-            height= 0.0,
-            width=0.0
+            title="test", image="test", base_price=0.0, max_amount_of_lettering_items=-1, height=0.0, width=0.0
         )
 
         number_of_categories = Category.objects.count()
 
-        self.assertEqual(
-            initial_number_of_categories+1,
-            number_of_categories
-        )
-    
+        self.assertEqual(initial_number_of_categories + 1, number_of_categories)
+
     def test_failure_create_category_without_title(self):
         test_category = Category(
-            title=None,                 
+            title=None,
             image="test",
             base_price=0.0,
             max_amount_of_lettering_items=-1,
@@ -38,5 +31,3 @@ class CategoryTestCase(TestCase):
 
         with self.assertRaises(ValidationError):
             test_category.full_clean()
-
-        
