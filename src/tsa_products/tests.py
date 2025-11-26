d1478e7ccf03e9d69f96b6311ebafe075d208f87
SarahZimmermann <sarah.zimmermann@neuronity.de>
2025-11-26 11:05:03 +0100

    chore: create test for order model

--- src/tsa_products/tests.py
+++ src/tsa_products/tests.py
@@ -1,7 +1,8 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
+from django.utils import timezone
 
-from tsa_products.models import Category, Comment, LetteringItemCategory, Product, ProductColor, ProductVariation, LetteringItemVariation
+from tsa_products.models import Category, LetteringItemCategory, Product, ProductColor, ProductVariation, LetteringItemVariation, Order, Comment
 
 # Create your tests here.
 
@@ -218,6 +219,49 @@ class LetteringItemVariationTestCase(TestCase):
             lettering_item_variation.save()
         self.assertEqual(LetteringItemVariation.objects.count(), 0)
 
+class OrderTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(self):
+        # Creates testing data for the test cases.
+        self.test_ordered_date = timezone.now()
+        self.test_user_email = "test-email"
+        self.test_user_first_name = "test-name"
+        self.test_user_last_name = "test-surname"
+        self.test_address1 = "Address line 1"
+        self.test_address2 = "Address line 2"
+        self.test_ordered = False
+        self.test_product = ProductVariation.objects.create(product=Product.objects.create(category=Category.objects.create(title="test", image="test-path"), title="test-title"))
+        self.test_comment = "test-comment"
+
+    def setUp(self):
+        # Ensures there are categories in the db before a test case is running.
+        Order.objects.all().delete()
+
+    def test_successful_order_creation(self):
+        # Tests the creation of an order.
+        order = Order.objects.create(
+            ordered_date = self.test_ordered_date,
+            user_email = self.test_user_email,
+            user_first_name = self.test_user_first_name,
+            user_last_name = self.test_user_last_name,
+            address1 = self.test_address1,
+            address2 = self.test_address2,
+            ordered = self.test_ordered,
+            product = self.test_product,
+            comment = self.test_comment
+        )
+        # Runs model validation for given data.
+        order.full_clean()
+        self.assertEqual(Order.objects.count(), 1)
+    
+    def test_failure_order_creation_without_user_email(self):
+        # Test the failure of order creation without an user_email.
+        with self.assertRaisesMessage(ValidationError, "{'user_email': ['This field cannot be blank.']}"):
+            order = Order(user_first_name = self.test_user_first_name)
+            order.full_clean()
+            order.save()
+        self.assertEqual(Order.objects.count(), 0)
 
 class CommentTestCase(TestCase):
 
